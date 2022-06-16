8b7168fa90324880bcfaaf60cfcb87cc0f03e6e5
Rogetz <ronnyogetaz@gmail.com>
2022-06-16 16:10:01 +0300

    aded two files here

--- ErrorWriteLineTest.cs
+++ ErrorWriteLineTest.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace ConsoleErrorWriteLineTest{
+internal namespace ConsoleErrorWriteLineTest{
     class ConsoleErrorWriteTest{
         static void Main(string[] args){
             /*It just works  the same way as the Console.WriteLine method */
---
+++ newTest.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace NewTest{
+    class NewTest{
+        static void Main(string[] args){
+            Console.WriteLine("Tester");
+        }
+    }
+}
